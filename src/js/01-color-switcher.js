d0860cba4775e47b97ab22a6590faca5c2a8d818
Kostiantyn Honcharov <goncharov.kostiantyn@gmail.com>
2022-06-04 11:01:38 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const refs = {
+    btnStart: document.querySelector("button[data-start]"),
+    btnStop: document.querySelector("button[data-stop]"),
+}
+
+let timerId = null;
+
+
+refs.btnStart.addEventListener('click', () => {
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+      getRandomHexColor();
+      timerId = setInterval(() => {
+      getRandomHexColor();    
+    }, 1000)
+})
+
+refs.btnStop.addEventListener('click', () => {
+    refs.btnStop.disabled = true;
+    refs.btnStart.disabled = false;
+    clearInterval(timerId);
+    console.log('dvfsd');
+})
+
+
+function getRandomHexColor() {
+  return document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
