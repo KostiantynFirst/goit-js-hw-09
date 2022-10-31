867f15e54b8ad1fb9da80e9f99b06698a65a4bcb
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2022-10-31 13:20:37 +0200

    Summary changes

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
 
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+const chageBodyColor = () => {
+    body.style.backgroundColor = getRandomHexColor();
+}
+
+const startSwitcher = () => {
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+    chageBodyColor();
+    timerId = setInterval(() => {
+        chageBodyColor();
+        console.log('startSwitcher')
+    }, 1000)
+}
+
+const stopSwitcher = () => {
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+    clearInterval(timerId);
+}
+
+buttonStart.addEventListener("click", startSwitcher);
+buttonStop.addEventListener("click", stopSwitcher);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
